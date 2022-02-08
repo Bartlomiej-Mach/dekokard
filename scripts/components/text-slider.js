9ad0ccdb436c5054993f080e07173aca902701ff
unknown <bartekmachu@gmail.com>
2022-02-08 10:44:30 +0100

    add text slider + add slides to img slider

---
+++ scripts/components/text-slider.js
@@ -0,0 +1,10 @@
+const swiper2 = new Swiper(".swiper2", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    direction: "vertical",
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
